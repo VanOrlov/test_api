473b77bec0b441a24782866a0322d4c90a6328ff
Ivan Orlov <mr.ivan.orlov1996@mail.ru>
2024-11-06 12:47:34 +0300

    Fixed middleware function - add next in arguments function

--- middleware/tokenRequired.js
+++ middleware/tokenRequired.js
@@ -1,4 +1,4 @@
-function tokenRequired(req, res){
+function tokenRequired(req, res, next){
     const token = req.headers['authorization']
     if(!token || token !== 'Bearer 0123456789'){
         return res.status(401).json({message: 'Token is not required'})
