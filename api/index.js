3534968d671411c55c45a551e39247b2f9600222
Ivan Orlov <mr.ivan.orlov1996@mail.ru>
2024-11-06 16:06:17 +0300

    1

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const tokenRequired = require('../middleware/tokenRequired');
-const handleWebhook = require('../handlers/webhookHandler')
 const fs = require('fs');
 const path = require('path');
 
